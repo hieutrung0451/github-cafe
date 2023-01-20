0a46e3c2f1fec2182fac99477eb66e3251fc903a
trunghieu <trunghieu0451@gmail.com>
2023-01-20 07:25:07 +0700

    get single user

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaGithub } from "react-icons/fa";
+import { BiCoffeeTogo } from "react-icons/bi";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
@@ -8,7 +8,7 @@ const Navbar = ({ title }) => {
     <nav className="navbar mb-12 shadow-lg bg-neutral text-neutral-content">
       <div className="container mx-auto">
         <div className="flex-none px-2 mx-2">
-          <FaGithub className="inline pr-2 text-3xl" />
+          <BiCoffeeTogo className="inline pr-2 text-3xl" />
           <Link to="/" className="text-lg font-bold align-middle">
             {title}
           </Link>
